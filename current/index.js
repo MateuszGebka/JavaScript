fbf7aaea904d9adb976b8cfa8d09f4ab8bd97260
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-06 07:15:42 +0100

    first commit

--- current/index.js
+++
@@ -1,7 +0,0 @@
-const email = "abcasdasd@gmail.com";
-
-let username = email.slice(0, email.indexOf("@"));
-let domain = email.slice(email.indexOf("@") + 1);
-
-console.log(username);
-console.log(domain);
