db91c3dbf1a63ad327c37028b68cbbac42e0ad88
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-21 17:15:50 +0100

    first commit

--- forEach/index.js
+++ forEach/index.js
@@ -1,10 +1,31 @@
 let numbers = [1, 2, 3, 4, 5];
 
 numbers.forEach(double);
-numbers.forEach(display);
+//numbers.forEach(display);
 
 function double(element, index, array){
-    array[index] = element*3;
+    array[index] = element*2;
+}
+
+/*
+function display(element){
+    console.log(element);
+}
+*/
+
+let fruit = ["apple", "orange", "banana"];
+
+//fruit.forEach(upper);
+fruit.forEach(upperFirst);
+fruit.forEach(display);
+
+
+function upper(element, index, array){
+    array[index] = element.toUpperCase();
+}
+
+function upperFirst(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
 }
 
 function display(element){
