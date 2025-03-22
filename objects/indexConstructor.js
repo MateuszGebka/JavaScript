ed68c76c86f4630d4cdc28cd313fe30b9c2f2a32
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-22 22:01:45 +0100

    fixes

--- objects/indexConstructor.js
+++ objects/indexConstructor.js
@@ -1,16 +1,19 @@
-function Car(make, model){
+function Car(make, model, year){
     this.make = make,
-    this.model = model
+    this.model = model,
+    this.year = year,
     this.drive = function(){console.log(`You drive the ${this.model}`)}
 }
 
-const car1 = new Car("Ford", "Mustang");
-const car2 = new Car("BMW", "Tiger");
+const car1 = new Car("Ford", "Mustang", 1212);
+const car2 = new Car("BMW", "Tiger", 313223);
 
 console.log(car1.make);
 console.log(car1.model);
+console.log(car1.year);
 car1.drive();
 
 console.log(car2.make);
 console.log(car2.model);
+console.log(car1.year);
 car2.drive();
