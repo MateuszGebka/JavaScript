10c03001191f31b26c3ef88f86f236e5f4cf0d58
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-22 22:49:16 +0100

    up

--- objects/indexConstructor.js
+++ objects/indexConstructor.js
@@ -7,6 +7,8 @@ function Car(make, model, year){
 
 const car1 = new Car("Ford", "Mustang", 1212);
 const car2 = new Car("BMW", "Tiger", 313223);
+const car3 = new Car("Audi", "SASD", 1);
+const car4 = new Car("Lambo", "Volvo", -122);
 
 console.log(car1.make);
 console.log(car1.model);
@@ -15,8 +17,18 @@ car1.drive();
 
 console.log(car2.make);
 console.log(car2.model);
-console.log(car1.year);
+console.log(car2.year);
 car2.drive();
+
+console.log(car3.make);
+console.log(car3.model);
+console.log(car3.year);
+car3.drive();
+
+console.log(car4.make);
+console.log(car4.model);
+console.log(car4.year);
+car4.drive();
 
 
 const drive = document.getElementById("drive");
