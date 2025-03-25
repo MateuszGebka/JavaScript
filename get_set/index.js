e756cceadc6cdca86d00f80d017a884d002608f7
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-25 21:44:16 +0100

    First commit

---
+++ get_set/index.js
@@ -0,0 +1,40 @@
+
+class Rectangle{
+    
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth){
+        if(newWidth > 0){
+            this._width = newWidth;
+        }
+        else{
+            console.error("Width must be a positive number");
+        }
+    }
+
+    set height(newHeight){
+        if(newHeight > 0){
+            this._height = newHeight;
+        }
+        else{
+            console.error("Height must be a positive number");
+        }
+    }
+
+    get width(){
+        return this._width;
+    }
+    
+    get height(){
+        return this._height;
+    }
+}
+
+const rectangle = new Rectangle(23, 43);
+
+console.log(rectangle.width);
+
+console.log(rectangle.height);
