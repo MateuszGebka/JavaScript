6dd99367c656a83f27a7f7c23000770a2c40e948
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-26 19:30:20 +0100

    closures

--- closures/index.js
+++ closures/index.js
@@ -23,17 +23,59 @@ function increment(){
         console.log(`Count increased to ${count}`);
     }
 
-    return {adder};
+    function getCount(){
+        return count;
+    }
+
+    return {adder, getCount};
 }
 
 const counter = increment();
 
 counter.adder();
-
+counter.adder();
+counter.adder();
+counter.adder();
 counter.adder();
 
-counter.adder();
+console.log(`The curr count is: ${counter.getCount()}`)
 
 counter.adder();
-
 counter.adder();
+counter.adder();
+
+console.log(`The curr count is: ${counter.getCount()}`)
+
+
+
+function createGame(){
+    let score = 0;
+
+
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points}pts`);
+    }
+
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+
+    function getScore(){
+        return score;
+    }
+
+    return {increaseScore, decreaseScore, getScore};
+}
+
+
+const game = new createGame();
+
+game.increaseScore(5);
+game.increaseScore(2);
+game.increaseScore(7);
+game.decreaseScore(1);
+game.decreaseScore(4);
+
+console.log(`Final score: ${game.getScore()}pts`);
