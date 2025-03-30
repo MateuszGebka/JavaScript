fd8302d52b079e0d8d29277257f8e030befcb7b1
MateuszGebka <mateuszgebka2001@o2.pl>
2025-03-30 22:56:29 +0200

    fix

--- dice roller/index.js
+++ dice roller/index.js
@@ -8,12 +8,13 @@ function rollDice(){
     const values = [];
     const images = [];
 
+    
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
         images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
     }
-    
+
 
     diceResult.textContent = `dices: ${values.join(', ')}`;
     diceImages.innerHTML = images.join('');
