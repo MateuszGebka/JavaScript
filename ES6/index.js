983687d475a9257e721ca45823d36e264847eb34
MateuszGebka <mateuszgebka2001@o2.pl>
2025-04-07 18:08:29 +0200

    first

---
+++ ES6/index.js
@@ -0,0 +1,11 @@
+import {getArea, getCirc, getVolume, PI} from './indexUtil.js'
+
+console.log(PI);
+
+const cir = getCirc(10);
+const area = getArea(10);
+
+
+console.log(`Circ: ${cir.toFixed(2)}`);
+
+console.log(`Area: ${area.toFixed(2)}`);
---
+++ ES6/indexUtil.js
@@ -0,0 +1,13 @@
+export const PI = 3.14;
+
+export function getCirc(radius){
+    return 2 * PI * radius;
+}
+
+export function getArea(radius){
+    return PI * radius * radius;
+}
+
+export function getVolume(radius){
+    return 4/3 * radius * radius * radius * PI;
+}
