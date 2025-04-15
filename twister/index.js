0367143be9f83c5f7cf6329001c10510517b70e8
MateuszGebka <mateuszgebka2001@o2.pl>
2025-04-15 12:18:21 +0200

    player names

--- twister/index.js
+++ twister/index.js
@@ -6,22 +6,50 @@ const playerName = document.getElementById("name");
 const titlePlayer = document.getElementById("name-select-h3");
 const nameSelection = document.getElementById("name-select");
 const startGameButton = document.getElementById("start-btn");
+const playersNames = document.getElementById("playersNames");
+
+const colors = ["Green", "Yellow", "Blue", "Red"];
+const part = ["Left arm", "Right arm", "Left leg", "Right leg"];
+
+let currentColor = -1;
+let currentPart = -1;
+
 
 function startGame(){
     input.value = "lol";
+    order = Math.floor(Math.random() * 1);
+    while(true){
+        setTimeout(function(){
+            currentColor = Math.floor(Math.random() * 4);
+            currentPart = Math.floor(Math.random() * 4);
+            input.value =
+
+        }, 3000);
+    }
 }
 
 function submitName(){
     if(names.length == 0){
-        names.push(playerName.value);
+        if(playerName.value === ""){
+            names.push("Player 1");
+        }
+        else{
+            names.push(playerName.value);
+        }
         playerName.value = "";
         console.log(names);
         titlePlayer.textContent = "Player 2:";
     }
     else if(names.length == 1){
-        names.push(playerName.value);
+        if(playerName.value === ""){
+            names.push("Player 2");
+        }
+        else{
+            names.push(playerName.value);
+        }
         console.log(names);
         nameSelection.style.display = "none";
         startGameButton.style.display = "flex";
+        playersNames.textContent = names;
     }
 }
