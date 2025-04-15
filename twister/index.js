10488a352e963d8b403d9c3d4d8f555d0e4acce6
MateuszGebka <mateuszgebka2001@o2.pl>
2025-04-15 11:55:36 +0200

    player names

--- twister/index.js
+++ twister/index.js
@@ -1,8 +1,17 @@
 
-let currentPlayer = 1;
-
+let currentPlayer = -1;
+let names = [];
 const input = document.getElementById("result");
+const playerName = document.getElementById("name");
 
 function startGame(){
     input.value = "lol";
+}
+
+function submitName(){
+    if(names.length == 0){
+        names.push(playerName.value);
+        playerName.value = "";
+        console.log(names);
+    }
 }
