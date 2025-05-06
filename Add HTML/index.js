238961acfa606e7d651f2e51c062bb9b290b9b15
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-06 16:39:59 +0200

    first

---
+++ Add HTML/index.js
@@ -0,0 +1,24 @@
+//      <h1>
+
+// STEP 1  CREATE THE ELEMENT
+
+const newH1 = document.createElement("h1");
+
+// STEP 2   ADD ATRIBUTES / PROPERTIES
+
+newH1.textContent = "Jestem nowym <h1>";
+newH1.id = "myH1";
+newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
+
+// STEP 3   APPEND ELEMENT TO DOM
+
+// document.body.append(newH1);  // na końcu
+
+// document.body.prepend(newH1);    // na początku
+
+// document.getElementById("box1").append(newH1);
+
+const box2 = document.getElementById("box2");
+
+document.body.insertBefore(newH1, box2);
