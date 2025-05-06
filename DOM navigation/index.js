ea97328e63000a2ba70d00dd51233b7e9d9af2ab
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-06 16:09:46 +0200

    first

---
+++ DOM navigation/index.js
@@ -0,0 +1,7 @@
+const element = document.getElementById("ul-1");
+
+console.log(element.firstElementChild.textContent);
+
+const firstElChild = element.firstElementChild;
+
+firstElChild.style.backgroundColor = "grey";
