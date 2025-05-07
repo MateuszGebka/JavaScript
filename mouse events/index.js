6e71f293dd4354c8ef5cbe1424e2965ac345ac1a
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-07 12:59:55 +0200

    first

--- mouse events/index.js
+++ mouse events/index.js
@@ -6,3 +6,11 @@ myBox.addEventListener("click", event => {
     event.target.style.backgroundColor = "yellow";   
     event.target.textContent = "Ouch"; 
 });
+
+myBox.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "green";
+})
+
+myBox.addEventListener("mouseout", event => {
+    event.target.style.backgroundColor = "lightgreen";
+})
