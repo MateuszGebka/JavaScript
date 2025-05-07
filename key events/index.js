c2b4e33f7a887db3bb925752ed2dfa5dea04dd69
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-07 13:18:10 +0200

    first

--- key events/index.js
+++ key events/index.js
@@ -1,8 +1,38 @@
 
 const myBox = document.getElementById("myBox");
+const moveAmount = 10;
+let x = 0;
+let y = 0;
 
 
-/*
+document.addEventListener("keydown", event => {
+    if(event.key.startsWith("Arrow")){
+
+        event.preventDefault();
+
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;    
+        }
+
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
+})
+
+
+
 document.addEventListener("keydown", event => {
     myBox.textContent = "Shocked";
     myBox.style.backgroundColor = "tomato";
@@ -14,5 +44,5 @@ document.addEventListener("keyup", event => {
     myBox.style.backgroundColor = "lightblue";
     console.log(`Key up = ${event.key}`);
 })
-*/
+
 
