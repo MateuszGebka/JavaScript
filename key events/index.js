68cd5a741a5e1f47c37b58e2010f15c143fd688e
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-07 13:12:58 +0200

    first:

--- key events/index.js
+++ key events/index.js
@@ -1,8 +1,18 @@
 
+const myBox = document.getElementById("myBox");
+
+
+/*
 document.addEventListener("keydown", event => {
+    myBox.textContent = "Shocked";
+    myBox.style.backgroundColor = "tomato";
     console.log(`Key down = ${event.key}`);
 })
 
 document.addEventListener("keyup", event => {
+    myBox.textContent = "Smiley";
+    myBox.style.backgroundColor = "lightblue";
     console.log(`Key up = ${event.key}`);
 })
+*/
+
