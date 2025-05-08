e039088b1db06e08520a7dffb4e573259870483a
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-08 15:06:57 +0200

    first

--- JSON/index.js
+++ JSON/index.js
@@ -5,7 +5,7 @@ const person = {
     "age": 18,
     "isEmployed": true,
     "hobbies": ["Jesc", "Spac", "ogladac tv"]
-}
+};
 const people = [{
     "name": "Adam",
     "age": 18,
@@ -25,7 +25,7 @@ const people = [{
     "name": "Dominik",
     "age": 2222,
     "isEmployed": true
-}]
+}];
 
 const jsonStringNames = JSON.stringify(names);
 const jsonStringPerson = JSON.stringify(person);
@@ -42,3 +42,48 @@ console.log(jsonStringPerson);
 
 console.log(people);
 console.log(jsonStringPeople);
+
+
+
+const jsonNames = `["Adam", "Beata", "Czarek", "Dominik"]`;
+const jsonPerson = `{
+    "name": "Adam",
+    "age": 18,
+    "isEmployed": true,
+    "hobbies": ["Jesc", "Spac", "ogladac tv"]
+}`;
+const jsonPeople = `[{
+    "name": "Adam",
+    "age": 18,
+    "isEmployed": true
+},
+{
+    "name": "Beata",
+    "age": 22,
+    "isEmployed": false
+},
+{
+    "name": "Czarek",
+    "age": 1,
+    "isEmployed": false
+},
+{
+    "name": "Dominik",
+    "age": 2222,
+    "isEmployed": true
+}]`
+
+const parsedNames = JSON.parse(jsonNames);
+
+console.log(jsonNames);
+console.log(parsedNames);
+
+const parsedPerson = JSON.parse(jsonPerson);
+
+console.log(jsonPerson);
+console.log(parsedPerson);
+
+const parsedPeople = JSON.parse(jsonPeople);
+
+console.log(jsonPeople);
+console.log(parsedPeople);
