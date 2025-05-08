bc3502015a4adec7bfc464754f1e325a39f0b099
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-08 13:34:17 +0200

    first

--- node lists/index.js
+++ node lists/index.js
@@ -8,3 +8,15 @@ buttons.forEach(button => {
     button.style.backgroundColor = "green";
     button.textContent += "LoL";
 });
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        if(event.target.style.backgroundColor === "tomato"){
+            event.target.style.backgroundColor = "blue";
+        }
+        else{
+            event.target.style.backgroundColor = "tomato";
+        }
+    });
+});
