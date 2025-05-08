c2bdea360f68e7e46c95ae08eae374f5d441765d
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-08 13:59:25 +0200

    first

--- classList/index.js
+++ classList/index.js
@@ -24,3 +24,34 @@ myBtn.addEventListener("click", event => {
     }
 
 });
+
+
+let buttons = document.querySelectorAll(".myBtns");
+
+buttons.forEach(button => {
+    button.classList.add("enabled");
+});
+
+
+buttons.forEach(button => {
+    button.addEventListener("mouseover", event => {
+        event.target.classList.toggle("hover");
+    })
+});
+
+buttons.forEach(button => {
+    button.addEventListener("mouseout", event => {
+        event.target.classList.toggle("hover");
+    })
+});
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        if(event.target.classList.contains("enabled")) {
+            event.target.classList.replace("enabled", "disabled");
+        }
+        else {
+            event.target.classList.replace("disabled", "enabled");
+        }
+    })
+});
