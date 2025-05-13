bb79ab82932419bd0b04a8b2d8320fb1499406fa
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-13 16:14:35 +0200

    first

--- fetch API/index.js
+++ fetch API/index.js
@@ -8,3 +8,24 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     })
     .then(data => console.log(data.abilities))
     .catch(error => console.error(error));
+
+
+
+async function fetchData() {     
+    try{
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/zorua");
+
+        if(!response.ok){
+            throw new Error("Could not fetch");
+        }
+
+            const data = await response.json();
+            console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    
+    }
+}
+
+fetchData();
