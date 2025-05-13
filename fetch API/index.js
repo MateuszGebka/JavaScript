667ccec853f5af3e4a1801a9b8f8f6d9b61b5f44
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-13 17:07:37 +0200

    first

--- fetch API/index.js
+++ fetch API/index.js
@@ -22,6 +22,7 @@ async function fetchData() {
         }
 
         const data = await response.json();
+        console.log(data);  
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
 
