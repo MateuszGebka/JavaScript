f99d30b303698588203880fa2ef6588fa96290ef
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-14 18:39:19 +0200

    fix

--- fetch API/index.js
+++ fetch API/index.js
@@ -27,13 +27,14 @@ async function fetchData() {
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
 
+        
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
+
         
     }
     catch(error){
         console.error(error);
-    
     }
 }
 
