f2e9458ec4f80ae5c310591d868be562c9327adc
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-22 09:22:41 +0200

    first

--- Algorithms/search/search.js
+++ Algorithms/search/search.js
@@ -1,3 +1,6 @@
+// LINEAR SEARCH //
+
+/*
 arr = [-5, 2, 10, 4, 6];
 t = 10;
 
@@ -11,3 +14,29 @@ function linearSearch(target, array){
 }
 
 console.log(linearSearch(t, arr));
+*/
+
+// BINARY SEARCH //
+
+array = [1, 3, 4, 7, 9, 11, 21];
+target = 11;
+
+function binarySearch(target, array){
+    let left = 0;
+    let right = array.length - 1;
+    while(left <= right){
+        let middle = Math.floor((left + right)/2);
+        if(array[middle] === target){
+            return middle;
+        }
+        else if(array[middle] > target){
+            right = middle - 1;
+        }
+        else if(array[middle] < target){
+            left = middle + 1;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch(2, array));
