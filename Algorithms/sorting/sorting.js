380f264f7ac07336115bd068644d3392f59836bb
MateuszGebka <mateuszgebka2001@o2.pl>
2025-05-23 10:03:49 +0200

    first

--- Algorithms/sorting/sorting.js
+++ Algorithms/sorting/sorting.js
@@ -28,6 +28,8 @@ console.log(array);
 // [-7, -1, 1, 4, 12, 7]
 // [-7, -1, 1, 4, 7, 12]
 
+
+/*
 let array = [4, -1, -7, 12, 1, 7];
 
 function insertionSort(array){
@@ -48,3 +50,32 @@ function insertionSort(array){
 console.log(array);
 insertionSort(array);
 console.log(array);
+*/
+
+
+// QUICK SORT //
+
+let array = [4, -1, -7, 12, 1, 7];
+
+function quickSort(array){
+    if(array.length < 2){
+        return array;
+    }
+    let pivot = array[array.length - 1];
+    let left = [];
+    let right = [];
+
+    for(let i = 0; i < array.length - 1; i++){
+        if(array[i] < pivot){
+            left.push(array[i]);
+        }
+        else{
+            right.push(array[i]); 
+        }
+    }
+
+    return [...quickSort(left), pivot,...quickSort(right)];
+}
+
+console.log(array);
+console.log(quickSort(array));
